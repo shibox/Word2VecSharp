63f2e3b6db03bd61242a5911b31e30758998091e
jarve <jarve@JARVEY>
2016-07-09 21:50:30 +0800

    添加项目文件。

---
+++ Word2VecSharp/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Word2VecSharp
+{
+    public class Class1
+    {
+    }
+}
